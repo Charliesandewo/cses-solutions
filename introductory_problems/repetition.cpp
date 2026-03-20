bf2bd6efa494a338a667fe93c2e0e8ef5b94cdfe
CharlieSandewo <sandewocharlie@gmail.com>
2026-03-20 18:02:15 +0700

    solution 3: repetition

--- introductory_problems/repetition.cpp
+++ introductory_problems/repetition.cpp
@@ -1,1 +1,27 @@
 #include <iostream>
+#include <string>
+
+int main(){
+  std::string input;
+  char current_letter;
+  int longest_repetition=0;
+  int current_repetition=0;
+
+  std::cin>>input;
+
+  for(char c: input){
+    if(current_letter != c){
+      current_letter = c;
+      current_repetition = 1;
+    }
+    else{
+      current_repetition++;
+    }
+    if(longest_repetition < current_repetition){
+      longest_repetition = current_repetition;
+    }
+  }
+
+  std::cout<<longest_repetition;
+  return 0;
+}
